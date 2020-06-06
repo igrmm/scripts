25ae5697e13b2689b28f3901f8553731b8d85a68
igrmm <igrmmdev@gmail.com>
2020-06-05 20:59:25 -0400

    push my scripts

---
+++ autorice.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+USAGE="\nUSAGE: ./autorice.sh --username=username --hostname=hostname
+       \nUSAGE: ./autorice.sh --username=username --hostname=hostname --post-installation\n"
+
+POST_INSTALLATION=false
+
+for i in "$@"; do
+	case $i in
+		-u=*|--username=*)
+			USERNAME="${i#*=}"
+			;;
+		-h=*|--hostname=*)
+			HOSTNAME="${i#*=}"
+			;;
+		--post-installation)
+			POST_INSTALLATION=true
+			;;
+	esac
+done
+
+if [ "$USERNAME" = "" ] || [ "$HOSTNAME" = "" ]; then
+	echo -e $USAGE
+	exit
+fi
+
+if [ ! -d /home/$USERNAME/.config ]; then
+    mkdir /home/$USERNAME/.config
+fi
+
+rm -rf /home/$USERNAME/.config/fontconfig
+ln -s /home/$USERNAME/dotfiles/fontconfig /home/$USERNAME/.config/fontconfig
+
+rm -rf /home/$USERNAME/.zshrc
+ln -s /home/$USERNAME/dotfiles/.zshrc /home/$USERNAME/.zshrc
+
+rm -rf /home/$USERNAME/.zprofile
+ln -s /home/$USERNAME/dotfiles/.zprofile /home/$USERNAME/.zprofile
+
+rm -rf /home/$USERNAME/.gtkrc-2.0
+ln -s /home/$USERNAME/dotfiles/.gtkrc-2.0 /home/$USERNAME/.gtkrc-2.0
+
+rm -rf /home/$USERNAME/.config/gtk-3.0
+ln -s /home/$USERNAME/dotfiles/gtk-3.0 /home/$USERNAME/.config/gtk-3.0
+
+rm -rf /home/$USERNAME/.config/i3blocks
+ln -s /home/$USERNAME/dotfiles/i3blocks/$HOSTNAME /home/$USERNAME/.config/i3blocks
+
+rm -rf /home/$USERNAME/.icons
+ln -s /home/$USERNAME/dotfiles/cursor.theme /home/$USERNAME/.icons
+
+rm -rf /home/$USERNAME/.config/termite
+ln -s /home/$USERNAME/dotfiles/termite /home/$USERNAME/.config/termite
+
+rm -rf /home/$USERNAME/.config/vifm
+ln -s /home/$USERNAME/dotfiles/vifm /home/$USERNAME/.config/vifm
+
+rm -rf /home/$USERNAME/.config/compton.conf
+ln -s /home/$USERNAME/dotfiles/compton/$HOSTNAME /home/$USERNAME/.config/compton.conf
+
+rm -rf /home/$USERNAME/.config/nvim
+ln -s /home/$USERNAME/dotfiles/nvim /home/$USERNAME/.config/nvim
+
+rm -rf /home/$USERNAME/.config/newsboat
+ln -s /home/$USERNAME/dotfiles/newsboat /home/$USERNAME/.config/newsboat
+
+rm -rf /home/$USERNAME/.config/sxiv
+ln -s /home/$USERNAME/dotfiles/sxiv /home/$USERNAME/.config/sxiv
+
+mkdir /home/$USERNAME/.local /home/$USERNAME/.local/share /home/$USERNAME/.local/share/applications
+ln -s /home/$USERNAME/dotfiles/mime/vifm.desktop /home/$USERNAME/.local/share/applications/vifm.desktop
+rm -rf /home/$USERNAME/.config/mimeapps.list
+ln -s /home/$USERNAME/dotfiles/mime/mimeapps.list /home/$USERNAME/.config/mimeapps.list
+
+rm .bash*
+
+if [ "$POST_INSTALLATION" = true ]; then
+	git clone https://aur.archlinux.org/yay.git
+	cd yay && makepkg -Asci && cd .. && rm -rf yay
+	yay -S archivemount rar2fs
+	sed -i 's/colorscheme gruvbox/"colorscheme gruvbox/g' /home/$USERNAME/dotfiles/nvim/init.vim
+	git clone https://github.com/VundleVim/Vundle.vim.git /home/$USERNAME/.vim/bundle/Vundle.vim
+	nvim +PluginInstall +qall
+	sed -i 's/"colorscheme gruvbox/colorscheme gruvbox/g' /home/$USERNAME/dotfiles/nvim/init.vim
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting .zsh/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-completions .zsh/zsh-completions
+	git clone https://github.com/zsh-users/zsh-history-substring-search .zsh/zsh-history-substring-search
+	git clone https://github.com/zsh-users/zsh-autosuggestions .zsh/zsh-autosuggestions
+	source /home/$USERNAME/.zprofile
+	if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
+		exec startx /home/$USERNAME/dotfiles/xinitrc
+	fi
+fi
---
+++ setuprpi.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+RICE=false
+
+SETUP_SD_CARD=false
+
+HOSTNAME="rbp"
+
+USAGE="\nUSAGE: ./setuprbp.sh --username=username --password=password
+       \nUSAGE: ./setuprbp.sh --rice --username=username --password=password
+       \nUSAGE: ./setuprbp.sh --set-up-sdcard\n"
+
+for i in "$@"; do
+	case $i in
+		-u=*|--username=*)
+			USERNAME="${i#*=}"
+			;;
+		-p=*|--password=*)
+			PASSWORD="${i#*=}"
+			;;
+		--rice)
+			RICE=true
+			;;
+		--set-sdcard)
+			SETUP_SD_CARD=true
+			;;
+	esac
+done
+
+if [ "$USERNAME" = "" ] || [ "$PASSWORD" = "" ]; then
+	echo -e $USAGE
+	exit
+fi
+
+if [ "$RICE" = true ] && [ "$SETUP_SD_CARD" = true ]; then
+	echo -e $USAGE
+	exit
+fi
+
+add_to_package_list() {
+	PACKAGE_LIST=$PACKAGE_LIST" $1"
+}
+
+#Essential
+add_to_package_list base-devel
+add_to_package_list sudo
+add_to_package_list xf86-video-fbturbo-git
+add_to_package_list xorg
+
+#Tools
+add_to_package_list neovim
+
+#Appearance
+add_to_package_list xcursor-bluecurv
+add_to_package_list papirus-icon-theme
+add_to_package_list arc-solid-gtk-theme
+
+#Fonts
+add_to_package_list ttf-inconsolata
+add_to_package_list ttf-croscore
+add_to_package_list noto-fonts-emoji
+add_to_package_list awesome-terminal-fonts
+
+set_up_sdcard() {
+	echo set_up_sdcard
+}
+
+rice() {
+	echo rice
+}
+
+if [ ! $RICE ] && [ ! $SETUP_SD_CARD]; then
+
+	loadkeys br-abnt2
+	pacman-key --init
+	pacman-key --populate archlinuxarm
+	timedatectl set-ntp true
+	ln -sf /usr/share/zoneinfo/America/Campo_Grande /etc/localtime
+	hwclock --systohc
+	echo pt_BR.UTF-8 UTF-8 >> /etc/locale.gen
+	echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+	locale-gen
+	echo LANG=pt_BR.UTF-8 > /etc/locale.conf
+	echo KEYMAP=br-abnt2 > /etc/vconsole.conf
+	echo $HOSTNAME > /etc/hostname
+	{ echo $PASSWORD; echo $PASSWORD; } | passwd
+
+	echo 'Section "InputClass"' > /etc/X11/xorg.conf.d/00-keyboard.conf
+	echo 'Identifier "system-keyboard"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+	echo 'MatchIsKeyboard "on"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+	echo 'Option "XkbLayout" "br"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+	echo 'Option "XkbModel" "abnt2"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+	echo 'Option "XkbVariant" "abnt2"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+	echo 'EndSection' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+
+	userdel -r alarm
+	useradd -m -G wheel $USERNAME
+	{ echo $PASSWORD; echo $PASSWORD; } | passwd $USERNAME
+	echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+	sed -i 's/#Color/Color/g' /etc/pacman.conf
+	pacman -Syyu
+	pacman --noconfirm --needed -S $PACKAGE_LIST
+
+	cd /home/$USERNAME/
+	git clone http://github.com/$USERNAME/scripts
+	git clone http://github.com/$USERNAME/dotfiles
+	chown $USERNAME -R /home/$USERNAME/
+	chgrp $USERNAME -R /home/$USERNAME/
+
+elif [ "$RICE" = true ]; then
+	rice
+fi
