58a7113ca342f43abcbadecff59d2c4ddcc41748
igrmm <igrmmdev@gmail.com>
2020-07-17 17:30:27 -0400

    add bspwm external rules script

---
+++ bspwm/external_rules.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+wid="$1"
+class="$2"
+instance="$3"
+
+WM_NAME() {
+	xprop -id "$wid" '\t$0' WM_NAME | cut -f 2
+}
+
+# FIREFOX
+[ "$class" = "firefox" ] && echo 'desktop=^10'
+
+# STEAM
+[ "$class" = "Steam" ] && echo 'desktop=^9'
+
+# THUNDERBIRD
+[ "$class" = "Thunderbird" ] && echo 'desktop=^8'
+
+# TELEGRAM
+[ "$instance" = "telegram-desktop" ] && \
+	case "$(WM_NAME)" in
+		'"Media viewer"')
+			;;
+		*)
+			echo 'state=floating rectangle=380x480+770+300'
+			;;
+	esac
