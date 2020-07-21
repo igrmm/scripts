0e8a13da5690ec6b689de9aa3dfda3a2af23545a
igrmm <igrmmdev@gmail.com>
2020-07-21 14:20:30 -0400

    send error to /dev/null in pacman script

--- bar/pacman.sh
+++ bar/pacman.sh
@@ -2,7 +2,7 @@
 
 fetch() {
 	if ! pgrep -x checkupdates >/dev/null; then
-		updates="$(checkupdates | wc -l)"
+		updates="$(checkupdates 2>/dev/null | wc -l)"
 		[ "$updates" -gt 0 ] && echo "$updates"
 	else
 		notify-send "checkupdates is already running."
