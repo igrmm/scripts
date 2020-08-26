73a276731a020909b559153af063d9b6de193331
igrmm <igrmmdev@gmail.com>
2020-08-26 18:43:43 -0400

    add basic arch linux installation script

---
+++ archinstall.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+printf 'hostname: ' && read -r HOSTNAME
+printf 'username: ' && read -r USERNAME
+printf 'password: ' && read -r PASSWORD
+printf 'linux partition: ' && read -r LINUX_PARTITION
+printf 'efi partition: ' && read -r EFI_PARTITION
+
+printf '\n'
+printf 'hostname:        %s\n' "$HOSTNAME"
+printf 'username:        %s\n' "$USERNAME"
+printf 'password:        %s\n' "$PASSWORD"
+printf 'linux partition: %s\n' "$LINUX_PARTITION"
+printf 'efi partition:   %s\n' "$EFI_PARTITION"
+printf '\n'
+printf 'Confirm? (y|n): ' && read -r CONFIRMATION
+
+case "$CONFIRMATION" in
+	y) ;;
+	n) echo "exit" && exit ;;
+	*) echo "exit" && exit ;;
+esac
+
+timedatectl set-ntp true
+
+echo y | mkfs.ext4 "$LINUX_PARTITION"
+mount "$LINUX_PARTITION" /mnt
+mkdir /mnt/boot
+mount "$EFI_PARTITION" /mnt/boot
+
+pacstrap /mnt base linux linux-firmware broadcom-wl iwd neovim
+genfstab -U /mnt >> /mnt/etc/fstab
+
+  cat <<EOF | arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/America/Campo_Grande /etc/localtime
+hwclock --systohc
+echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+
+echo "$HOSTNAME" > /etc/hostname
+
+{ echo "$PASSWORD"; echo "$PASSWORD"; } | passwd
+
+bootctl install
+
+sed -i 's/#Color/Color/g' /etc/pacman.conf
+
+exit
+EOF
+
+umount -R /mnt
