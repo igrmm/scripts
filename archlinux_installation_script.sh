968f5c98dc3ad7524841d8136ff7649a36c51193
igrmm <igrmmdev@gmail.com>
2020-08-30 19:31:19 +0000

    update description

--- archlinux_installation_script.sh
+++ archlinux_installation_script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Personal arch linux installation script. This uses EFISTUB for booting directly
-# into the kernel, so no boot loader. This script uses only 1 disk and 2 partitions
-# the first for EFI (esp) and second for root.
+# into the kernel, so no boot loader. This script uses only 1 disk and 3 partitions
+# the first for EFI (esp), second for swap and last for root.
 
 printf 'hostname: '            && read -r HOSTNAME
 printf 'username: '            && read -r USERNAME
