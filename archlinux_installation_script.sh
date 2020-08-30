75f15122061075da542a4da787e45f6b1494998b
igrmm <igrmmdev@gmail.com>
2020-08-30 16:23:00 -0400

    add iwd dhcp configuration

--- archlinux_installation_script.sh
+++ archlinux_installation_script.sh
@@ -67,6 +67,9 @@ systemctl enable systemd-networkd.service
 systemctl enable systemd-resolved.service
 systemctl enable iwd.service
 
+echo "[General] > /etc/iwd/main.conf
+echo "EnableNetworkConfiguration=true" >> /etc/iwd/main.conf
+
 exit
 EOF
 
