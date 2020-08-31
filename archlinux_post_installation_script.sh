8752cb48fd306b9d18ecfcb3db6cdf6aa91805f2
igrmm <igrmmdev@gmail.com>
2020-08-31 11:46:38 -0400

    add username and password creation and basic packages

--- archlinux_post_installation_script.sh
+++ archlinux_post_installation_script.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-pacman -S bspwm
+printf 'username: ' && read -r USERNAME
+printf 'password: ' && read -r PASSWORD
+
+printf '\n'
+printf 'username: %s\n' "$USERNAME"
+printf 'password: %s\n' "$PASSWORD"
+printf '\n'
+printf 'Confirm? (y|n) ' && read -r CONFIRMATION
+
+case "$CONFIRMATION" in
+	y) ;;
+	n) echo "exit" && exit ;;
+	*) echo "exit" && exit ;;
+esac
+
+pacman -S base-devel xorg-xinit man-db man-pages nvidia-settings sudo \
+	alsa-utils
+
+useradd -m -G wheel "$USERNAME"
+
+{ echo "$PASSWORD"; echo "$PASSWORD"; } | passwd "$USERNAME"
+
+
