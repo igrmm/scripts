9de7a378fbec55887a5126e56a869ceb2bc50b89
igrmm <igrmmdev@gmail.com>
2023-09-26 20:54:51 -0400

    fix telegram script: bring window to focus

--- bspwm/launchtelegram.sh
+++ bspwm/launchtelegram.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-[ -n "$(xtitle $(bspc query -N -n .window) | grep Telegram)" ] \
-	&& xdo activate -n "telegram-desktop" \
-	|| telegram-desktop
+WINDOWS=$(bspc query -N -n .window)
+
+for wid in $WINDOWS
+do
+    if xprop -id "$wid" | grep -q telegram; then
+        xdo activate -n "telegram-desktop"
+        exit
+    fi
+done
+telegram-desktop
